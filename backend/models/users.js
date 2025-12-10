55ae0c9c624b17850f2b8855cc36ef87e58d645f
Zhan <e.leozhan@icloud.com>
2025-12-10 15:06:07 +0100

    modele user

---
+++ backend/models/users.js
@@ -0,0 +1,13 @@
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false },
+    role: { 
+      type: DataTypes.ENUM("USER", "ADMIN"),
+      defaultValue: "USER"
+    }
+  });
+
+  return User;
+};
