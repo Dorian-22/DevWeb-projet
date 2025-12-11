22af16235e671aae2b264e13ba1dddeb250b8690
Dorian <dorianperrot92@gmail.com>
2025-12-11 16:01:28 +0100

    modification compose.yml

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,24 +1,36 @@
+// lib/db.js
 const { Sequelize } = require('sequelize');
 
-const DB_NAME = process.env.DB_NAME || 'eventsdb';
-const DB_USER = process.env.DB_USER || 'myuser';
-const DB_PASSWORD = process.env.DB_PASSWORD || 'mypassword';
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_PORT = process.env.DB_PORT || 5432;
+let sequelize;
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  port: DB_PORT,
-  dialect: 'postgres',
-  logging: false, 
-});
+if (process.env.DATABASE_URL) {
+  // Cas Docker : on utilise l'URL complète
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    logging: false,
+  });
+} else {
+  // Cas dev local : on utilise les variables séparées
+  const DB_NAME = process.env.DB_NAME || 'eventsdb';
+  const DB_USER = process.env.DB_USER || 'myuser';
+  const DB_PASSWORD = process.env.DB_PASSWORD || 'mypassword';
+  const DB_HOST = process.env.DB_HOST || 'localhost';
+  const DB_PORT = process.env.DB_PORT || 5432;
+
+  sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: 'postgres',
+    logging: false,
+  });
+}
 
 async function testConnection() {
   try {
     await sequelize.authenticate();
-    console.log('Connection to database has been established successfully.');
+    console.log(' Connection to database has been established successfully.');
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error(' Unable to connect to the database:', error);
   }
 }
 
